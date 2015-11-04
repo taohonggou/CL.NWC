caba6ab76f726b540066f3a53687f458a50bf8c6
taohonggou <cl889521@163.com>
2015-11-04 18:34:21 +0800

    添加了实体

---
+++ CL.NWC.Model/ContactRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL.NWC.Model
+{
+    public class ContactRecord
+    {
+        public int RecordID { get; set; }
+        public int UserID { get; set; }
+        public int ProjectID { get; set; }
+        public string Content { get; set; }
+        public DateTime RecordDate { get; set; }
+    }
+}
---
+++ CL.NWC.Model/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL.NWC.Model
+{
+    public class Project
+    {
+        public int ProjectID { get; set; }
+        public int UserID { get; set; }
+        public string ForemanName { get; set; }
+        public string Phone { get; set; }
+        public int Schedule { get; set; }
+        public string Address { get; set; }
+        public DateTime RecordDate { get; set; }
+        public DateTime ShuiDate { get; set; }
+        public DateTime NiDate { get; set; }
+        public DateTime MuDate { get; set; }
+        public DateTime YouDate { get; set; }
+        public DateTime PredictCompleteDate { get; set; }
+    }
+}
