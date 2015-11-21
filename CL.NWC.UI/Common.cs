9ed65acb2fdd74b1d2e473332ffc3dcff5adc2ca
taohonggou <cl889521@163.com>
2015-11-21 18:10:03 +0800

    基本功能完成了

---
+++ CL.NWC.UI/Common.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CL.NWC.BLL;
+using CL.NWC.Model;
+using System.Windows.Forms;
+
+namespace CL.NWC.UI
+{
+    public class Common
+    {
+        public static void MessageBoxShow(string msg)
+        {
+            MessageBox.Show(msg);
+            return;
+        }
+
+        public static void MessageBoxShow(string msg,bool isReturn)
+        {
+            MessageBox.Show(msg);
+            if (isReturn)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ CL.NWC.UI/frmEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL.NWC.UI
+{
+    public class frmEventArgs:EventArgs
+    {
+        public object obj { get; set; }
+        public int flag { get; set; }
+    }
+}
